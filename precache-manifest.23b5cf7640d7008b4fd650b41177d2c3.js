c9ef0d7f1d7e6baed23a9ad3912794b166cde517
Krista Clarke <kristaclarke82@gmail.com>
2020-08-21 13:22:29 -0700

    Updates

---
+++ precache-manifest.23b5cf7640d7008b4fd650b41177d2c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbe607beb75ad1551613",
+    "url": "/my_clicky_game/static/css/main.220e362d.chunk.css"
+  },
+  {
+    "revision": "dbe607beb75ad1551613",
+    "url": "/my_clicky_game/static/js/main.dbe607be.chunk.js"
+  },
+  {
+    "revision": "d49f461ddd43f0b563e9",
+    "url": "/my_clicky_game/static/js/1.d49f461d.chunk.js"
+  },
+  {
+    "revision": "27b8748d50edaa052350",
+    "url": "/my_clicky_game/static/js/runtime~main.27b8748d.js"
+  },
+  {
+    "revision": "87de69c3f2f4d380db4711b6b9ce7266",
+    "url": "/my_clicky_game/static/media/minions_banner2.87de69c3.jpg"
+  },
+  {
+    "revision": "55de1264dccf7da75ef8997ca2660a1e",
+    "url": "/my_clicky_game/index.html"
+  }
+];
--- src/components/Jumbotron/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import "./style.css";
-
-const Jumbotron = props => (
-<div className="jumbotron"></div>
-);
-
-export default Jumbotron;
